417cc700a3d2f7c525dcdbfbbec6a582d8604ac0
quezixiong <zixiongque003236@sohu-inc.com>
2017-03-23 17:18:38 +0800

    integrate RabbitMQ

---
+++ src/main/java/com/kuaizhan/config/MqConfig.java
@@ -0,0 +1,10 @@
+package com.kuaizhan.config;
+
+/**
+ * MQ的config类
+ * 定义发送到MQ的routing key常量，这个值也和queue name一致
+ * Created by zixiong on 2017/3/23.
+ */
+public class MqConfig {
+    public static final String IMPORT_KUAIZHAN_POST = "sys-weixin-import-kuaizhan-post";
+}
