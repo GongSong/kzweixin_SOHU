1d958595915328111a933acc0e73a0e1213d4170
刘成(媒体产品技术中心) <chengliu@sohu-inc.com>
2017-04-17 15:33:53 +0800

    Add deploy scripts

--- deploy/web/build.sh
+++ deploy/web/build.sh
@@ -1,3 +1,24 @@
-#mvn clean package -Dmaven.test.skip=true && \
-docker build -t registry.kuaizhan.com/kuaizhan-plf/weixin-web ./deploy/ && \
-docker push registry.kuaizhan.com/kuaizhan-plf/weixin-web
+build_image() {
+    env=$1
+    tag=$2
+
+    mvn clean package -Dmaven.test.skip=true -P ${env} && \
+    docker build -t ${tag} -f ./deploy/web/Dockerfile . && \
+    docker push ${tag}
+}
+
+ENV=$1
+case "${ENV}" in
+    'dev')
+        build_image dev registry.kuaizhan.com/kuaizhan-plf/weixin-web:dev
+    ;;
+    'test')
+        build_image test registry.kuaizhan.com/kuaizhan-plf/weixin-web:test
+    ;;
+    'pre')
+        build_image production private-registry.sohucs.com/kuaizhan-plf/weixin-web:pre
+    ;;
+    'prod')
+        build_image production private-registry.sohucs.com/kuaizhan-plf/weixin-web:prod
+    ;;
+esac
--- deploy/web/init-tpl.sh
+++ deploy/web/init-tpl.sh
@@ -3,23 +3,4 @@ source ~/.bashrc
 cd $JETTY_BASE
 
 PORT1={{.Env.AUTO_PORT0}}
-MYIP={{.Env.DOMEOS_SERVER_ADDR}}
-SERVICE_NAME=kzweixin
-ID=${SERVICE_NAME}-${MYIP}-${PORT1}
-HCURL=http://${MYIP}:${PORT1}/kzweixin/v1/common/ping
-CONSUL_URL=http://consul.kuaizhan.sohuno.com
-
-curl -X PUT -d "{\
-  \"ID\": \"${ID}\",\
-  \"Name\": \"${SERVICE_NAME}\",\
-  \"Address\": \"${MYIP}\",\
-  \"Port\": ${PORT1},\
-  \"EnableTagOverride\": false,\
-  \"Check\": {\
-    \"DeregisterCriticalServiceAfter\": \"1m\",\
-    \"HTTP\": \"${HCURL}\",\
-    \"Interval\": \"5s\"\
-  }\
-}" $CONSUL_URL/v1/agent/service/register
-
-java -jar $JETTY_HOME/start.jar
+java -jar $JETTY_HOME/start.jar jetty.http.port=$PORT1
