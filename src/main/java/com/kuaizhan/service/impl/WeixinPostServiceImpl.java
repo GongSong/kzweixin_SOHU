a33fbe7d65747a8f7d035e0873c1e81f68b00190
quezixiong <harleyque@sohu-inc.com>
2017-04-19 16:45:55 +0800

    close the annoying mongo log;

--- src/main/java/com/kuaizhan/service/impl/WeixinPostServiceImpl.java
+++ src/main/java/com/kuaizhan/service/impl/WeixinPostServiceImpl.java
@@ -57,8 +57,8 @@ public class WeixinPostServiceImpl implements WeixinPostService {
         imgUrl = UrlUtil.fixProtocol(imgUrl);
         // 获取内部地址
         Map<String ,String> address = UrlUtil.getPicIntranetAddress(imgUrl);
+        logger.info("[微信] 上传图片素材, address: " + address);
         String result = HttpClientUtil.postMedia(ApiConfig.addMaterialUrl(accessToken, "image"), address.get("url"), address.get("host"));
-        logger.info("[微信] 上传图片素材, address: " + address + " result: " + result);
 
         JSONObject returnJson = new JSONObject(result);
         if (returnJson.has("errcode")) {
