753e6f4089448a0b6dff639fb6685b7c86f52399
quezixiong <harleyque@sohu-inc.com>
2017-04-26 10:05:39 +0800

    fix KZPicUploadException inherit from BaseException ;

--- src/main/java/com/kuaizhan/exception/business/KZPicUploadException.java
+++ src/main/java/com/kuaizhan/exception/business/KZPicUploadException.java
@@ -6,7 +6,7 @@ import com.kuaizhan.exception.BaseException;
 /**
  * Created by lorin on 17-3-30.
  */
-public class KZPicUploadException extends BaseException {
+public class KZPicUploadException extends BusinessException {
     public KZPicUploadException() {
         super(ErrorCodeConfig.KZPIC_UPLOAD_ERROR.getCode(), ErrorCodeConfig.KZPIC_UPLOAD_ERROR.getMsg());
     }
