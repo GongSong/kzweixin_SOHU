a0bd6629c3a1080d0597eb87463051149d583856
fangtianyu <tianyufang@sohu-inc.com>
2017-07-05 12:00:54 +0800

    rename SubscribeConsumer

--- src/main/java/com/kuaizhan/kzweixin/mq/consumer/SubscribeConsumer.java
+++ src/main/java/com/kuaizhan/kzweixin/mq/consumer/SubscribeConsumer.java
@@ -1,6 +1,6 @@
 package com.kuaizhan.kzweixin.mq.consumer;
 
-import com.kuaizhan.kzweixin.mq.dto.AfterSubscribeDTO;
+import com.kuaizhan.kzweixin.mq.dto.SubscribeDTO;
 import com.kuaizhan.kzweixin.service.FanService;
 import com.kuaizhan.kzweixin.utils.JsonUtil;
 
@@ -9,14 +9,14 @@ import javax.annotation.Resource;
 /**
  * Created by fangtianyu on 7/5/17.
  */
-public class AfterSubscribeConsumer extends BaseConsumer{
+public class SubscribeConsumer extends BaseConsumer{
 
     @Resource
     private FanService fanService;
 
     @Override
     void onMessage(String message) {
-        AfterSubscribeDTO dto = JsonUtil.string2Bean(message, AfterSubscribeDTO.class);
+        SubscribeDTO dto = JsonUtil.string2Bean(message, SubscribeDTO.class);
         fanService.addFanOpenId(dto.getAppId(), dto.getOpenId());
         fanService.addFan(dto.getAppId(), dto.getOpenId());
     }
--- src/main/java/com/kuaizhan/kzweixin/mq/dto/SubscribeDTO.java
+++ src/main/java/com/kuaizhan/kzweixin/mq/dto/SubscribeDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
  * Created by fangtianyu on 7/5/17.
  */
 @Data
-public class AfterSubscribeDTO {
+public class SubscribeDTO {
     private String appId;
     private String openId;
 }
