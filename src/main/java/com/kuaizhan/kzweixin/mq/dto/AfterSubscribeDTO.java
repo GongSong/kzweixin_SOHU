7384d013041a45a419df7e32dd689908bb097c75
fangtianyu <tianyufang@sohu-inc.com>
2017-07-05 11:48:11 +0800

    create AfterSubscribeConsumer by RabbitMQ

---
+++ src/main/java/com/kuaizhan/kzweixin/mq/dto/AfterSubscribeDTO.java
@@ -0,0 +1,12 @@
+package com.kuaizhan.kzweixin.mq.dto;
+
+import lombok.Data;
+
+/**
+ * Created by fangtianyu on 7/5/17.
+ */
+@Data
+public class AfterSubscribeDTO {
+    private String appId;
+    private String openId;
+}
