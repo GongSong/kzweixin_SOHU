5fdb0c447a78d68f7a2556b8c6f5a46ae45898b2
quezixiong <harleyque@sohu-inc.com>
2017-07-11 13:12:46 +0800

    remove emojis in fan nickname;

--- src/main/java/com/kuaizhan/kzweixin/service/impl/FanServiceImpl.java
+++ src/main/java/com/kuaizhan/kzweixin/service/impl/FanServiceImpl.java
@@ -14,10 +14,7 @@ import com.kuaizhan.kzweixin.manager.WxFanManager;
 import com.kuaizhan.kzweixin.mq.dto.SubscribeDTO;
 import com.kuaizhan.kzweixin.service.AccountService;
 import com.kuaizhan.kzweixin.service.FanService;
-import com.kuaizhan.kzweixin.utils.DBTableUtil;
-import com.kuaizhan.kzweixin.utils.DateUtil;
-import com.kuaizhan.kzweixin.utils.JsonUtil;
-import com.kuaizhan.kzweixin.utils.MqUtil;
+import com.kuaizhan.kzweixin.utils.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -314,7 +311,7 @@ public class FanServiceImpl implements FanService {
         fanPO.setAppId(appId);
         fanPO.setOpenId(openId);
         fanPO.setStatus(userInfoDTO.getSubscribe() == 1 ? 1 : 2);
-        fanPO.setNickName(userInfoDTO.getNickName());
+        fanPO.setNickName(StrUtil.removeEmojis(userInfoDTO.getNickName()));
         fanPO.setCity(userInfoDTO.getCity());
         fanPO.setProvince(userInfoDTO.getProvince());
         fanPO.setCountry(userInfoDTO.getCountry());
