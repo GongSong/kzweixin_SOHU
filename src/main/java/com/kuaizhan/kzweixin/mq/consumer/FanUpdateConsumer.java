8a27a6211716c2c7a3f047927eae2f8d0f46e464
fangtianyu <tianyufang@sohu-inc.com>
2017-07-13 15:02:20 +0800

    test

--- src/main/java/com/kuaizhan/kzweixin/mq/consumer/FanUpdateConsumer.java
+++ src/main/java/com/kuaizhan/kzweixin/mq/consumer/FanUpdateConsumer.java
@@ -24,8 +24,13 @@ public class FanUpdateConsumer extends BaseConsumer{
         fanService.addFanOpenId(appId, openId);
 
         FanPO fanPO = fanService.getFanByOpenId(appId, openId);
-
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("[FanUpdateConsumer]fanPO fanId:" + fanPO.getFanId() + " fanNickname:" + fanPO.getNickName());
+        System.out.println("DateUtil.curSeconds():" + DateUtil.curSeconds());
+        System.out.println("fanPO.getUpdateTime():" + fanPO.getUpdateTime());
+        System.out.println(DateUtil.curSeconds() - fanPO.getUpdateTime());
         if (fanPO == null || DateUtil.curSeconds() - fanPO.getUpdateTime() > 3600) {
+            System.out.println("##############################in the if clause#################################");
             fanService.refreshFan(appId, openId);
         }
     }
