eabb2c33d2478746b56aac0a435b542d01d66c0f
杨笑轩(产品技术/实习生) <xiaoxuanyang003977@sohu-inc.com>
2017-07-20 14:40:39 +0800

    wxinternal

--- src/main/java/com/kuaizhan/kzweixin/manager/WxInternalManager.java
+++ src/main/java/com/kuaizhan/kzweixin/manager/WxInternalManager.java
@@ -50,14 +50,10 @@ public class WxInternalManager {
     /**
      * 删除定时任务接口
      * @param jobName string 任务名（id）
-     * @param jobUrl string 定时执行的任务接口
-     * @param startTime int 任务执行时间(10位时间戳)
      */
-    public static void deleteTimingJob(String jobName, String jobUrl, long startTime) {
+    public static void deleteTimingJob(String jobName) {
         Map<String, String> param = new HashMap<>();
         param.put("name", jobName);
-        param.put("url", jobUrl);
-        param.put("start", String.valueOf(startTime * 1000));
         String paramStr = JsonUtil.bean2String(param);
         String result = HttpClientUtil.postJson(getDeleteJobUrl(), paramStr);
         if (result == null) {
