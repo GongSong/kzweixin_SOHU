94c1c87cd50feccfcf6e19fcf3f4108c56cc6ff9
quezixiong <harleyque@sohu-inc.com>
2017-08-02 14:19:12 +0800

    fix ComponentResponseType;

--- src/main/java/com/kuaizhan/kzweixin/enums/ComponentResponseType.java
+++ src/main/java/com/kuaizhan/kzweixin/enums/ComponentResponseType.java
@@ -4,11 +4,12 @@ package com.kuaizhan.kzweixin.enums;
  * Created by zixiong on 2017/08/01.
  */
 public enum ComponentResponseType implements BaseEnum {
-    POST(1),
-    PAGE(2),
-    TEXT(3),
-    IMAGE(4),
-    LINK_GROUP(5);
+
+    POST(1), // 图文列表
+    // 值为2的"页面"已经被废弃
+    TEXT(3), // 文本
+    IMAGE(4), // 图片
+    LINK_GROUP(5); // 链接组
 
     private int value;
 
